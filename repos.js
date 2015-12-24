4989e6b916be8ee475cfde7c79940772b1836ee2
Richard Russon <rich@flatcap.org>
2015-12-24 11:58:42 +0000

    neomutt

--- repos.js
+++ repos.js
@@ -38,6 +38,38 @@ $(document).ready (function() {
 		}
 	});
 
+	var neomutt_table = $('#neomutt_table');
+
+	$.ajax ({
+		dataType: "json",
+		url: 'https://api.github.com/users/neomutt/repos',
+
+		success: function (json) {
+			var repos = [];
+			$.each (json, function (index, repo) {
+				repos.push (repo);
+			});
+			repos.sort (repo_sort);
+
+			$.each (repos, function (index, repo) {
+				if (repo.fork) {
+					return true;
+				}
+				var desc = repo.description.replace (/https*:\/\/.+/g, "<a href=\"$&\">$&</a>");
+
+				var row = '';
+				row += '<td>' + repo.created_at.substr(0,10) + '</td>';
+				row += '<td><a href="' + repo.html_url + '">' + repo.name + '</a></td>';
+				row += '<td>' + desc + '</td>';
+
+				neomutt_table.append ('<tr>'+row+'</tr>');
+			});
+		},
+		error: function (jq, jqStatus, jqError) {
+			alert ('error');
+		}
+	});
+
 	var gists_table = $('#gists_table');
 
 	$.ajax ({
